ee6382e761aa038d1c8bd684fb56b9ecbcc7c2b3
Bastian Mueller <bastian@turbolent.com>
2024-03-30 08:11:18 -0700

    backport String.end_with?, Ruby 1.8.2 doesn't have it yet

--- gen_bridge_metadata.rb
+++ gen_bridge_metadata.rb
@@ -2339,3 +2339,17 @@ if __FILE__ == $0
 	end
     end
 end
+
+unless String.method_defined? :end_with?
+  class String
+    def end_with?(*suffixes)
+      suffixes.any? do |suffix|
+        if suffix.respond_to? :to_str
+          suffix = suffix.to_str
+          self[-suffix.length, suffix.length] == suffix
+        end
+      end
+    end
+  end
+end
+
