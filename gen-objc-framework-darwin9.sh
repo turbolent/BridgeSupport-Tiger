449bc39df9fee56faf0595a0088c4fd340d48027
Bastian Mueller <bastian@turbolent.com>
2024-04-27 23:00:31 -0700

    generate objc in complete format

--- gen-objc-framework-darwin9.sh
+++ gen-objc-framework-darwin9.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p DSTROOT-darwin9.0/System/Library/Frameworks/objc.framework/Versions/A/Resources/BridgeSupport
-RUBYLIB=DSTROOT-darwin9.0/System/Library/BridgeSupport/ruby-1.8 ruby gen_bridge_metadata.rb  -c '-I/usr/include/objc' -C '-I/usr/include/objc' /usr/include/objc/objc.h /usr/include/objc/runtime.h /usr/include/objc/message.h -o DSTROOT-darwin9.0/System/Library/Frameworks/objc.framework/Versions/A/Resources/BridgeSupport/objc.bridgesupport
+RUBYLIB=DSTROOT-darwin9.0/System/Library/BridgeSupport/ruby-1.8 ruby gen_bridge_metadata.rb -F complete -c '-I/usr/include/objc' -C '-I/usr/include/objc' /usr/include/objc/objc.h /usr/include/objc/runtime.h /usr/include/objc/message.h -o DSTROOT-darwin9.0/System/Library/Frameworks/objc.framework/Versions/A/Resources/BridgeSupport/objc.bridgesupport
